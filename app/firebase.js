17ae991104c6d26af1833949c0b9f7f7e4dd68f8
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-08-14 00:28:10 +0800

    something

--- app/firebase.js
+++ app/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXZZtP307n-2sFhEexepzRKOsS66HwSX8",
-  authDomain: "chat-support-ab0b8.firebaseapp.com",
-  projectId: "chat-support-ab0b8",
-  storageBucket: "chat-support-ab0b8.appspot.com",
-  messagingSenderId: "782581149650",
-  appId: "1:782581149650:web:ec02a3c39da22abd87e20b"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
