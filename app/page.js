a413f1b30f359d1bf2880bdcf8e5407f9d8bf087
Arpit Srivastav <arpitsriv28@gmail.com>
2024-08-15 01:56:10 +0300

    Improved Homepage UI

--- app/page.js
+++ app/page.js
@@ -2,11 +2,40 @@
 import React, { useState } from "react"
 import Navbar from "./components/Navbar"
 import { AuthProvider } from "./context/authContext"
-
+import Box from "@mui/material/Box"; 
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
     return (
         <AuthProvider>
         <Navbar page={"Home"}/>
-        </AuthProvider>)
+        <Box
+            sx={{
+                marginTop: '40px', 
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '24px',
+                textAlign: 'center',
+            }}
+            >
+            <Typography variant="h4" gutterBottom sx={{ fontSize: '4rem', fontWeight:'bold' }}>
+                Welcome to Consumer Compass!
+            </Typography>
+            <Box
+                component="img"
+                src="./logo.webp" 
+                alt="Consumer Compass Logo"
+                sx={{ width: '150px', height: 'auto', marginBottom: '16px' }} 
+            />
+            <Typography variant="body1" sx={{ maxWidth: '800px', marginTop: '16px', fontSize: '1.5rem' }}>
+                This chatbot is designed to help you navigate the complex world of consumer rights in Ontario. 
+                Whether you're dealing with a tricky refund, need guidance on warranties, or want to know your rights when making a purchase, 
+                our chatbot is here to provide quick and accurate information tailored to Ontario's regulations. 
+                Get started by signing up (or logging in if you have an existing account) from the menu.
+            </Typography>
+        </Box>
+        </AuthProvider>
+    )   
 };
